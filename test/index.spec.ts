a842a99fba507f065e7ef2827cfc65dfcd0385fa
34j <55338215+34j@users.noreply.github.com>
2024-03-24 16:47:55 +0900

    test: remove test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,2 @@
-import { myPackage } from '../src';
-
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
-
-      const result = myPackage(message);
-
-      expect(result).toMatch(message);
-    });
-  });
-});
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+test.skip('skip', () => {});
